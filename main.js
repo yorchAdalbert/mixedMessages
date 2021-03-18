822fd706320c773c45495be32c4b897e6dfc8bd7
Jorge Adalberto <george_nlc@hotmail.com>
2021-03-17 22:01:12 -0600

    added an empty data-structure to the program

---
+++ main.js
@@ -0,0 +1,13 @@
+/* Mixed Messages...
+ * Create a ramdom message releated to today fortune.
+ * Everytime the user run the program, a new random message is gonna 
+ * log in the terminal...
+ */
+
+// @uthor: Yorch_Adalberto
+
+const dayWeather = {};
+const things = {};
+const avoidingThings = {};
+
+console.log(dayWeather);
