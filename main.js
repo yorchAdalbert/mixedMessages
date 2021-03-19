b6b63fd2a0eb494270824cb44efeabf2f13ab06f
yorch_Adalbert <georgeyberenice@gmail.com>
2021-03-18 22:19:08 -0600

    The program is working properly
    The code needs to be refactored.

--- main.js
+++ main.js
@@ -13,4 +13,22 @@ const cloaths = ['hat or a cap', 'umbrella', 'jacket', 'raincoat', 'sweater',
 const placesToGo = ['park', 'home', 'forest', 'shelter', 'mountain', 'downtown',
                     'restaurant', 'movie theater', 'lake', 'beach'];
 
-console.log(dayWeather);
+//const randomDayFurtune = {dayWeather, cloaths, placesToGo};
+
+const getRandomIndex = array => {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return randomIndex;
+};
+
+const day = dayWeather[getRandomIndex(dayWeather)];
+const cloath = cloaths[getRandomIndex(cloaths)];
+const place = placesToGo[getRandomIndex(placesToGo)];
+
+const furtuneDayMessage = 
+`!Welcome!
+Are you ready to know your furtune day?
+Today's weather is ${day},
+You should use a ${cloath},
+and the best place to go is to a ${place}.`
+
+console.log(furtuneDayMessage);
